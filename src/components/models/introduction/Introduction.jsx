75618ee7c88ca8ea262bb935c67cdcd7df828f74
unknown <partharora557@gmail.com>
2023-06-08 21:39:53 +0530

    done

--- src/components/models/introduction/Introduction.jsx
+++ src/components/models/introduction/Introduction.jsx
@@ -25,7 +25,7 @@ const Introduction = () => {
       <div className="h-[42px] rounded-[30px] bg-[#f17238]  px-[30px] min-w-[300px] flex items-center justify-center text-[20px] font-medium capitalize text-[#fff] ">
         <span className="translate-y-[1px]">Introduction</span>
       </div>
-      <div className="pb-[30px] pt-[20px] overflow-hidden  scroll-hidden w-[1300px] h-[550px] shrink-0 bg-[#ffffff90] backdrop-blur-lg mt-[20px] mb-[10px] rounded-[20px]">
+      <div className="pb-[30px] pt-[20px] overflow-hidden  scroll-hidden w-[100%] h-[100%]  bg-[#ffffff90] backdrop-blur-lg mt-[20px] ">
         <div className="w-[100%] flex items-center h-[40px] justify-center absolute top-0 ">
           <img
             onClick={() => {
@@ -60,7 +60,7 @@ const Introduction = () => {
         {curr === 7 && <MetaverseBiggest trans={trans} />}
         {curr === 8 && <ServicesAlready trans={trans} />}
       </div>
-      <div className="w-[100%] h-[100px] flex items-center justify-center rotate-180 cursor-pointer">
+      <div className="w-[100%] h-[100px] absolute bottom-0 z-50 flex items-center justify-center rotate-180 cursor-pointer">
         <img
           src="/arrow.svg"
           onClick={() => {
